4049421733ab4c5edd6fddee6f6ef741d724c1c2
Gayathri3214 <122510999+Gayathri3214@users.noreply.github.com>
2023-10-15 23:06:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import react  from 'react';
+import Card from './component/card/card'
+import './App.css'
+    
+ 
+class App extends react.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            apiData:[],
+            loader:true
+         }
+    }
+  componentDidMount() {
+    fetch('https://www.omdbapi.com/?apikey=45f0782a&s=war')
+    .then(response => response.json())
+    .then(json => this.setState({apiData:json.Search,loader:false}))
+  
+  }
+    render() { 
+        let {loader,apiData}=this.state;
+        console.log('searchdata',apiData);
+        return ( <div>
+   <h2 className="heading">Movies</h2>
+    <div className="container">
+    <div className="Wrapper">
+{ loader?<h1>Loading....</h1>:
+      apiData?.map((item,key)=>
+          <Card item={item} key={key}/>
+      )}
+
+    
+    </div>
+    </div>
+  
+        </div> );
+    }
+}
+ 
+export default App;
